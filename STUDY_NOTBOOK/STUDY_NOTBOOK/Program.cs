83a3f53b6e8685337104da461c5eb0b93f71235f
김성필 <ksp@kspat.com>
2020-06-03 17:02:18 +0900

    foreach문

--- STUDY_NOTBOOK/STUDY_NOTBOOK/Program.cs
+++ STUDY_NOTBOOK/STUDY_NOTBOOK/Program.cs
@@ -1,18 +1,34 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace STUDY_NOTBOOK
 {
 
-
     class Program
     {
         static void Main(string[] args)
         {
-
+            List<char> KeyList = new List<char>();
+            ConsoleKeyInfo keyval = new ConsoleKeyInfo();
+            
+            do
+            {
+                keyval = Console.ReadKey();
+                KeyList.Add(keyval.KeyChar);
+            } while (keyval.Key != ConsoleKey.Q);
+            {
+                Console.WriteLine();
+                foreach (var item in KeyList)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+                        
         }
     }
 }
